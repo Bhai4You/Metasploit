f6d08b8cd257bd350e3b554cd6a5ecef20b2899a
Sutariya Parixit <32614362+Bhai4You@users.noreply.github.com>
2017-10-08 21:55:35 +0530

    kingsploit.sh

---
+++ kingsploit.sh
@@ -0,0 +1,62 @@
+#!/data/data/com.termux/files/usr/bin/bash
+echo "##############################################"
+
+echo "##############################################"
+
+	figlet -f big Bhai 4 You
+
+echo "##############################################"
+
+echo "##############################################"
+
+
+apt install -y autoconf bison clang coreutils curl findutils git apr apr-util libffi-dev libgmp-dev libpcap-dev postgresql-dev readline-dev libsqlite-dev openssl-dev libtool libxml2-dev libxslt-dev ncurses-dev pkg-config postgresql-contrib wget make ruby-dev libgrpc-dev termux-tools ncurses-utils ncurses unzip zip tar postgresql
+echo "####################################"
+apt update && apt upgrade
+echo "Downloading & Extracting....."
+
+cd $HOME
+curl -LO https://github.com/rapid7/metasploit-framework/archive/4.16.4.tar.gz
+tar -xf $HOME/4.16.4.tar.gz
+mv $HOME/metasploit-framework-4.16.4 $HOME/metasploit-framework
+rm $HOME/4.16.4.tar.gz
+cd $HOME/metasploit-framework
+sed '/rbnacl/d' -i Gemfile.lock
+sed '/rbnacl/d' -i metasploit-framework.gemspec
+gem install bundler
+sed 's|nokogiri (1.*)|nokogiri (1.8.0)|g' -i Gemfile.lock
+
+gem install nokogiri -v 1.8.0 -- --use-system-libraries
+ 
+sed 's|grpc (.*|grpc (1.4.1)|g' -i $HOME/metasploit-framework/Gemfile.lock
+gem unpack grpc -v 1.4.1
+cd grpc-1.4.1
+curl -LO https://raw.githubusercontent.com/grpc/grpc/v1.4.1/grpc.gemspec
+curl -L https://wiki.termux.com/images/b/bf/Grpc_extconf.patch -o extconf.patch
+patch -p1 < extconf.patch
+gem build grpc.gemspec
+gem install grpc-1.4.1.gem
+cd ..
+rm -r grpc-1.4.1
+
+
+cd $HOME/metasploit-framework
+bundle install -j5
+$PREFIX/bin/find -type f -executable -exec termux-fix-shebang \{\} \;
+rm ./modules/auxiliary/gather/http_pdf_authors.rb
+ln -s $HOME/metasploit-framework/msfconsole /data/data/com.termux/files/usr/bin/
+echo "###############################"
+echo "##############################################"
+figlet -f small Sutariya Parixit
+echo "###############################"
+echo "###############################################"
+
+
+
+echo "###############################"
+echo "###############################"
+echo "Visit http://bhai4you.blogspot.com"
+echo "###############################"
+echo "####################################"
+echo " NOW YOU CAN LAUNCH METASPLOIT BY JUST EXECUTE THE COMMAND :=> ./msfconsole"
+echo "####################################"
